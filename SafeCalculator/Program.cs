780ec465b765fe09e3ba01d7255b96bad0f227f7
DESKTOP-NJGSL36\ok623 <hyunwoo.nam@digipen.edu>
2026-03-13 18:13:35 +0900

    작성 중

--- SafeCalculator/Program.cs
+++ SafeCalculator/Program.cs
@@ -1,4 +1,27 @@
+#nullable enable
 using System;
 
 // README.md를 읽고 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+
+SafeCalculator calc = new SafeCalculator();
+
+string? input1 = Console.ReadLine();
+string? input2 = Console.ReadLine();
+
+Console.WriteLine("=== 테스트 1: 정상 입력 ===");
+
+calc.Divide(input1, input2);
+
+Console.WriteLine("\n=== 테스트 2: 0으로 나누기 ===");
+
+input1 = Console.ReadLine();
+input2 = Console.ReadLine();
+
+calc.Divide(input1, input2);
+
+Console.WriteLine("\n=== 테스트 3: 잘못된 형식 ===");
+
+input1 = Console.ReadLine();
+input2 = Console.ReadLine();
+
+calc.Divide(input1, input2);
---
+++ SafeCalculator/SafeCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class SafeCalculator
+{
+    public void Divide(string num1, string num2)
+    {
+        try
+        {
+            int result = int.Parse(num1) / int.Parse(num2);
+
+            Console.WriteLine($"{num1} / {num2} = {result}");
+        }
+        catch(FormatException)
+        {
+            Console.WriteLine("올바른 숫자를 입력하세요.");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("0으로 나눌 수 없습니다.");
+        }
+        finally
+        {
+            Console.WriteLine("계산기를 종료합니다.");
+        }
+    }
+
+}
+
